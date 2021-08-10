a8733eb19d538821c48ec27df29998eb168ad6ee
Nguyễn Huỳnh Đức <nguyenhuynhduc96vp@gmail.com>
2021-08-10 13:39:47 +0700

    add bt

---
+++ Modifier/src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        Circle circle = new Circle(5.0);
+        System.out.println("Radius : " + circle.getRadius());
+        System.out.println("Area : " + circle.getArea());
+    }
+}
